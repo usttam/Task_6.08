daf4ba950b7c15a710366c80bdca4d429cbf171c
usttam <tamulinas.ustinas@hotmail.com>
2022-11-29 18:00:04 +0300

    add style.css for task5, add 2 more divs(TRAFFIC LIGHT)& logic work update script in task3

--- task3/script.js
+++ task3/script.js
@@ -1,18 +1,40 @@
-const trafficLightEl = document.querySelector('#trafficLight');
+const trafficLightEl = document.querySelector('#trafficLight-green');
+const trafficLightE2 = document.querySelector('#trafficLight-yellow');
+const trafficLightE3 = document.querySelector('#trafficLight-red');
 
 function makeGreen() {
     trafficLightEl.style.background = ('green');
+    trafficLightE2.style.background = ('black'); 
+    trafficLightE3.style.background = ('black'); 
     trafficLightEl.removeEventListener('click', makeGreen);
+    trafficLightE2.removeEventListener('click', makeGreen);
+    trafficLightE3.removeEventListener('click', makeGreen);
+    trafficLightE2.addEventListener('click', makeYellow);
     trafficLightEl.addEventListener('click', makeYellow);
+    trafficLightE3.addEventListener('click', makeYellow);
 }
 function makeYellow() {
-    trafficLightEl.style.background = ('yellow');
+    trafficLightE2.style.background = ('yellow');
+    trafficLightEl.style.background = ('black'); 
+    trafficLightE3.style.background = ('black');   
     trafficLightEl.removeEventListener('click', makeYellow);
+    trafficLightE2.removeEventListener('click', makeYellow);
+    trafficLightE3.removeEventListener('click', makeYellow);
+    trafficLightE2.addEventListener('click', makeRed);
     trafficLightEl.addEventListener('click', makeRed);
+    trafficLightE3.addEventListener('click', makeRed);
 }
 function makeRed() {
-    trafficLightEl.style.background = ('red');
+    trafficLightE2.style.background = ('black');
+    trafficLightEl.style.background = ('black'); 
+    trafficLightE3.style.background = ('red');   
     trafficLightEl.removeEventListener('click', makeRed);
+    trafficLightE2.removeEventListener('click', makeRed);
+    trafficLightE3.removeEventListener('click', makeRed);
+    trafficLightE2.addEventListener('click', makeGreen);
     trafficLightEl.addEventListener('click', makeGreen);
+    trafficLightE3.addEventListener('click', makeGreen);
 }
 trafficLightEl.addEventListener('click', makeGreen);
+trafficLightE2.addEventListener('click', makeGreen);
+trafficLightE3.addEventListener('click', makeGreen);
